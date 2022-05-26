3e570377342776b61aa4e7ea8f92f521384e6693
naravich@enres.co <seagame@MacBook-Pro-3.local>
2022-05-26 13:43:54 +0700

    [01] - print

--- 01-print/main.go
+++ 01-print/main.go
@@ -3,8 +3,12 @@ package main
 import "fmt"
 
 func main() {
+	var brand = "Google"
+	fmt.Printf("%q\n", brand)
+	fmt.Printf("%s\n", brand)
 	ops, ok, fail := 2350, 543, 433
 
+	fmt.Println("hi\nhi\\h")
 	fmt.Println(
 		"total:", ops, "- success:", ok, "/", fail,
 	)
@@ -13,4 +17,11 @@ func main() {
 		"total: %d - success: %d / %d\n",
 		ops, ok, fail,
 	)
+
+	var speed int
+	var heat float64
+	var off bool
+	fmt.Printf("speed type : %T \n", speed)
+	fmt.Printf("heat type : %T \n", heat)
+	fmt.Printf("off type : %T \n", off)
 }

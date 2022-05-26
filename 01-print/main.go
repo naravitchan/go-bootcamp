c9ba9dd71aa90e8818a6acf83ba9d680de023942
naravich@enres.co <seagame@MacBook-Pro-3.local>
2022-05-26 15:30:04 +0700

    [01] - print

--- 01-print/main.go
+++ 01-print/main.go
@@ -24,4 +24,23 @@ func main() {
 	fmt.Printf("speed type : %T \n", speed)
 	fmt.Printf("heat type : %T \n", heat)
 	fmt.Printf("off type : %T \n", off)
+
+	var (
+		planet   = "venus"
+		distance = 261
+		orbital  = 224.701
+		hasLife  = false
+	)
+	fmt.Printf("Planet : %v \n", planet)
+	fmt.Printf("distance : %v \n", distance)
+	fmt.Printf("orbital : %v \n", orbital)
+	fmt.Printf("Does %v have life? %v \n", planet, hasLife)
+
+	fmt.Printf("---- cast type safety ----\n")
+	fmt.Printf("Planet : %s \n", planet)
+	fmt.Printf("distance : %d \n", distance)
+	fmt.Printf("orbital : %f \n", orbital)
+	fmt.Printf("orbital : %.0f \n", orbital)
+	fmt.Printf("orbital : %.10f \n", orbital)
+	fmt.Printf("Does %s have life? %t \n", planet, hasLife)
 }

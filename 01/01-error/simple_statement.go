aeccbcc16fba8b4545dfd2ccccf38e069c8017a1
naravich@enres.co <seagame@MacBook-Pro-3.local>
2022-05-27 11:07:21 +0700

    [01] - short if with shadow

--- 01/01-error/simple_statement.go
+++ 01/01-error/simple_statement.go
@@ -7,11 +7,15 @@ import (
 )
 
 func main() {
+	var n int
+	var err error
+	n = 1
 	if a := os.Args; len(a) != 2 {
 		fmt.Println("give me a number.")
-	} else if _, err := strconv.Atoi(a[1]); err != nil {
-		fmt.Println("cannot conv.")
+	} else if n, err = strconv.Atoi(a[1]); err != nil {
+		fmt.Println("cannot conv.", a[1], n)
 	} else {
 		fmt.Println("complete")
 	}
+	fmt.Println("n : ", n)
 }
